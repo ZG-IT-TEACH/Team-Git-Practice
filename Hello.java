788568564ed74ad3f508743369f8e8aeb65de26b
weexploit <1063915758@qq.com>
2018-04-10 16:46:52 +0800

    提交简单java代码

---
+++ Hello.java
@@ -0,0 +1,12 @@
+package Hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		System.out.println("I am T7 weexploit");
+		// TODO Auto-generated method stub
+
+	}
+
+}
